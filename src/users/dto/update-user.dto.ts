e8b86469ec311aeab8b88954a8e125a29e903ec9
Raphaël A <azevedo.raphael2017@gmail.com>
2026-05-27 11:55:26 +0200

    UpdateDto updated using partialType and omit way

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,33 +1,6 @@
-import { IsEmail, Length, Matches, IsString, IsOptional } from "class-validator";
+import { OmitType, PartialType } from "@nestjs/mapped-types";
+import { CreateUserDto } from "./create-user.dto";
 
-export class UpdateUserDto {
-    @IsOptional()
-    @Length(3, 20, {
-        message: 'Le nom d\'utilisateur doit contenir entre 3 et 20 caractères.',
-    })
-    @IsString({
-        message: 'Le nom d\'utilisateur doit être une chaîne de caractères.',
-    })
-    username: string;
-
-    @IsOptional()
-    @IsString({
-        message: 'L\'adresse email doit être une chaîne de caractères.',
-    })
-    @IsEmail(undefined, {
-        message: 'Veuillez fournir une adresse email valide.',
-    })
-    email: string;
-    
-    @IsOptional()
-    @Length(6, 100, {
-        message: 'Le mot de passe doit contenir au moins 6 caractères.',
-    })
-    @IsString({
-        message: 'Le mot de passe doit être une chaîne de caractères.',
-    })
-    @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/, {
-        message: 'Le mot de passe doit contenir au moins 6 caractères, dont au moins une lettre et un chiffre.',
-    })
-    password: string;
-}
+export class UpdateUserDto extends PartialType(
+    OmitType(CreateUserDto, ['email'] as const)
+) {}
